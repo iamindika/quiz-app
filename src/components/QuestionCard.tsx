351380388eaa7e535de34a64f834ab0756126566
iamindika <ikolamba2@gmail.com>
2021-08-12 13:55:45 -0400

    Define typings for props

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,3 +1,19 @@
-const QuestionCard = () => <div>Question Card</div>;
+type Props = {
+  question: string;
+  answer: string[];
+  callback: any;
+  userAnswer: string;
+  questionNumber: number;
+  totalQuestions: number;
+}
+
+const QuestionCard: React.FC<Props> = ({ 
+  question, 
+  answer, 
+  callback, 
+  userAnswer, 
+  questionNumber, 
+  totalQuestions
+}) => <div>Question Card</div>;
 
 export default QuestionCard;
